238534f129810ec5902f616da423bc641889611a
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-09 16:09:42 -0500

    Adds code, light tests, and html

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const testVar = {}
+const input = document.querySelector('input');
 
-function testFunc() {
-  return "hi"
+function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
+
+function preventingDefault() {
+  input.addEventListener('keydown', function(e) {
+    if (e.which === 71) {
+      return e.preventDefault();
+    } else {
+       return 'Not a "g"'
+    }
+  });
+}
+
+function stoppingPropagation(){
+  const divs = document.querySelectorAll('div');
+  function bubble(e) {
+    e.stopPropagation();
+  
+    console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+  }
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', bubble);
+  }
+}
+
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,16 @@
 
 
 describe("index.html", () => {
-  it("includes 'hello'", () => {
-    expect(document.querySelector('body').innerHTML).to.include('hello')
-  })
-
   it("includes div", () => {
-    expect(document.querySelector('body').innerHTML, "no empty div element found within body").to.include('<div></div>')
+    expect(document.querySelector('body').innerHTML).to.include('<div>')
   })
-
-
 })
 
 
 describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-    expect(testFunc).to.exist
-    expect(testFunc()).to.equal('hi')
+  it("has each codealong function", () => {
+    expect(addingEventListener).to.exist
+    expect(preventingDefault).to.exist
+    expect(stoppingPropagation).to.exist
   })
 })
