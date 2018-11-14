9ddaea87efad8881fd0a74eb023b1d2d04a1699d
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-11-14 13:49:56 -0500

    Updates README and code for Mod3

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
+const input = document.querySelector('input');
+
 function addingEventListener() {
+  input.addEventListener('click', function(event) {
+    return "clicked"
+  });
 }
+
+function preventingDefault() {
+  input.addEventListener('keydown', function(e) {
+    if (e.which === 71) {
+      console.log('default prevented')
+      return e.preventDefault()
+    } else {
+      console.log('Not a "g"')
+    }
+  })
+}
+
+function stoppingPropagation(){
+  const divs = document.querySelectorAll('div');
+  function bubble(e) {
+    e.stopPropagation();
+  
+    console.log(this.firstChild.nodeValue.trim() + ' bubbled');
+  }
+  for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener('click', bubble);
+  }
+}
+
