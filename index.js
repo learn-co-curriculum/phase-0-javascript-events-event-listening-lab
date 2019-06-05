f2a9cd8cbc085d74e08750c652ce3e9b6f4fb93a
Steven G. Harms <steven.harms@flatironschool.com>
2019-06-05 13:39:57 -0400

    remove extra functions

--- index.js
+++ index.js
@@ -5,27 +5,3 @@ function addingEventListener() {
     return "clicked"
   });
 }
-
-function preventingDefault() {
-  input.addEventListener('keydown', function(e) {
-    if (e.which === 71) {
-      console.log('default prevented')
-      return e.preventDefault()
-    } else {
-      console.log('Not a "g"')
-    }
-  })
-}
-
-function stoppingPropagation(){
-  const divs = document.querySelectorAll('div');
-  function bubble(e) {
-    e.stopPropagation();
-  
-    console.log(this.firstChild.nodeValue.trim() + ' bubbled');
-  }
-  for (let i = 0; i < divs.length; i++) {
-    divs[i].addEventListener('click', bubble);
-  }
-}
-
