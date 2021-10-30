beb89953cee69eae86a7d05768821e82fa5f1cff
cssmith07060 <cssmith07060@gmail.com>
2021-10-30 06:07:07 -0400

    commit tree 74365812f7b54acd2a1a9e391da885c70b7cee66

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    input.addEventListener()
 }
