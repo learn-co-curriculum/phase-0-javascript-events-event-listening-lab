58a6bbb4945969d2d90a5c30ef7bbf0d1d3631bd
Paola Moreno Reyes <nailamoreno99@gmail.com>
2021-11-14 14:29:14 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-function addingEventListener() {
+function addingEventListener(){
+    const input = document.getElementById('input');
+
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert); 
 }
+
+addingEventListener;
+clickAlert;
