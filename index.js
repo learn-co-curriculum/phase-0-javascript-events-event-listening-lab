797db8f7aa93c354fc0b2767bb5c4813f0f9fd8b
Paola Moreno Reyes <nailamoreno99@gmail.com>
2022-02-07 17:39:35 -0600

    commit tree 48cc3edcb8e3f3ec59f7002015472d13a3f96fe9

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
-function addingEventListener(){
-    const input = document.getElementById('input');
+function moveDodgerLeft() {
 
-    function clickAlert() {
-      alert('I was clicked!');
-    }
-    
-    input.addEventListener('click', clickAlert); 
+  const dodger = document.getElementById("dodger");
+  const leftNumber = dodger.style.left.replace ("px","");
+  const left = parsInt(leftNumber, 10);
+
+  if (left > 0) {
+    dodger.style.left= `${left - 1}px`;
+  }
+
+}
+function moveDodgerRight() {
+  const dodger = document.getElementById("dodger");
+  const leftNumber= dodger.style.left.replace("px","");
+  const right = parseInt (leftNumber, 10);
+
+  if (right < 380) {
+    dogder.style.left = `${right + 1}px`; 
+  }
 }
 
-addingEventListener;
-clickAlert;
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }
+});
+document.addEventListener("keydown", function (e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight();
+  }
+});
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,15 +1,1 @@
-const sinon = require('sinon');
-
-describe("index.js", () => {
-  let input;
-
-  beforeEach(function() {
-    input = document.getElementById('input');
-    sinon.spy(input, 'addEventListener');
-  })
-
-  it("binds an event listener in addingEventListener()", () => {
-    addingEventListener();
-    expect(input.addEventListener.called).to.be.true;
-  })
-})
+const exampleVariable = 5;
