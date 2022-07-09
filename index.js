9bddff5dab5655f84ff42e3f0ad2b8ba60891061
katya2108 <katerinavodeiko@gmail.com>
2022-07-09 00:23:05 -0600

    commit tree 1b79c7cd83e253b96989b48629496a73796fb203

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
 function addingEventListener() {
+input.addEventListener('click', clickAlert);
 }
