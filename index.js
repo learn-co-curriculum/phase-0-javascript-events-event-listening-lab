ed19b4b769097b3a08f42be42e2c37ccd6d8f882
Sean <ohsean812@gmail.com>
2023-06-20 23:11:44 -0400

    commit tree bd77686296d2352b6ef6ce381811c43c8a09ef2b

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
+    const input = document.getElementById("button")
+    input.addEventListener('click', function() {
+        alert("I was clicked!")
+    })
 }
+
+addingEventListener();
